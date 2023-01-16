c69025114e58eeeac0ec909e3e0d4d75fb9a9721
andersonsol <andersonocodomundo@gmail.com>
2023-01-16 15:22:43 -0300

    alteração realizada

--- trilha-java-basico/java-anatomia-classes/src/BoletimEstudantil.java
+++ trilha-java-basico/java-anatomia-classes/src/BoletimEstudantil.java
@@ -4,7 +4,7 @@ public class BoletimEstudantil{
      * @param args
      */
     public static void main(String[]args) {
-        int mediaFinal = 6;
+        final int mediaFinal = 7;
         if (mediaFinal < 6)
             System.out.println("REPROVADO");
         else if (mediaFinal == 6);
