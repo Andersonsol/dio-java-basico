2a4bffc39a37968f3e7abf0e5bec7de0891d0302
andersonsol <andersonocodomundo@gmail.com>
2023-01-16 08:10:24 -0300

    boletim estuantil incluindo

---
+++ trilha-java-basico/java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+public class BoletimEstudantil{
+
+    /**
+     * @param args
+     */
+    public static void main(String[]args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6);
+            System.out.println("PPROVA MINERVA");
+        else 
+            System.out.println("APROVADO");
+    }
+}
